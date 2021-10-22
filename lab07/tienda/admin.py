2a2bb100dc150ff8f0c17ccb7637c98b866f3356
EstefaniDavila <estefani.davila@tecsup.edu.pe>
2021-10-22 13:50:24 -0500

    lab09-2

--- lab07/tienda/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-from .models import   Producto, categoria
-
-admin.site.register(categoria)
-admin.site.register(Producto)
--- lab5/tienda/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-# Register your models here.
-from .models import  Cliente, Producto, categoria
-
-admin.site.register(categoria)
-admin.site.register(Producto)
-admin.site.register(Cliente)
